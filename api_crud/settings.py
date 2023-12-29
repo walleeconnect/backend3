e6d5cb909ad7168433d42e87fb5fe6d88c4ec732
walleeconnect <walleeconnect@gmail.com>
2023-12-29 14:55:01 +0530

    mg44

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-   # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
     'authentication',
@@ -161,3 +161,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static'
