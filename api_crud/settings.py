14ebaa405b71f4a31ece05eecd0a9b8ea75e13df
walleeconnect <walleeconnect@gmail.com>
2023-12-29 14:50:14 +0530

    mg4

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+   # 'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
     'authentication',
